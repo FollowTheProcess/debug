54af947beb0c5f1c41478c1da21cc832493278d0
Tom Fleet <tomfleet2018@gmail.com>
2024-01-17 16:32:15 +0000

    Initial impl of Debug (#1)

--- debug_test.go
+++ debug_test.go
@@ -1,16 +1,74 @@
 package debug_test
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
 	"testing"
 
 	"github.com/FollowTheProcess/debug"
+	"github.com/FollowTheProcess/test"
 )
 
-func TestHello(t *testing.T) {
-	got := debug.Hello()
-	want := "Hello debug"
+var (
+	anInt   = 2
+	aBool   = true
+	aString = "hello world"
+)
 
-	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
+func TestDebug(t *testing.T) {
+	_, file, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("could not get runtime caller")
 	}
+
+	runDebug := func() {
+		debug.Debug(anInt)
+		debug.Debug(aBool)
+		debug.Debug(aString)
+	}
+
+	out := captureStdout(t, runDebug)
+
+	want := fmt.Sprintf(`DEBUG: [%[1]s:28:3] anInt = 2
+DEBUG: [%[1]s:29:3] aBool = true
+DEBUG: [%[1]s:30:3] aString = hello world
+`, file)
+
+	test.Diff(t, out, want)
+}
+
+func captureStdout(t *testing.T, printer func()) string {
+	t.Helper()
+	old := os.Stdout // Backup of the real one
+	defer func() {
+		os.Stdout = old // Set it back even if we error later
+	}()
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("os.Pipe() returned an error: %v", err)
+	}
+
+	// Set stdout to our new pipe
+	os.Stdout = w
+
+	capture := make(chan string)
+	// Copy in a goroutine so printing can't block forever
+	go func() {
+		buf := new(bytes.Buffer)
+		io.Copy(buf, r) //nolint: errcheck
+		capture <- buf.String()
+	}()
+
+	// Call our test function that prints to stdout
+	printer()
+
+	// Close the writer
+	w.Close()
+	captured := <-capture
+
+	return captured
 }
